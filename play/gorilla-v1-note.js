9396850a2082013a4cc81bc6f089f3e34440e9e4
flatoutratout <flatoutwithyaratout@gmail.com>
2026-08-29 12:57:53 +0100

    Stage Gorilla V1 isolated asset checkpoint

---
+++ play/gorilla-v1-note.js
@@ -0,0 +1,2 @@
+// Gorilla V1 style checkpoint is being staged on this branch. Actual raster asset replacement follows in the next commit; no other game art is modified.
+window.RR_GORILLA_V1_BRANCH = true;
