30b76b32ba3aca42d017e4d6591a37d9a4f30e31
flatoutratout <flatoutwithyaratout@gmail.com>
2026-08-31 01:41:50 +0100

    Add V2 game configuration

---
+++ v2/config.js
@@ -0,0 +1,23 @@
+window.RR2 = {
+  width: 960,
+  height: 540,
+  floorY: 468,
+  worldWidth: 120000,
+  assetRoot: '../play/assets/',
+  characters: {
+    gorilla: { name: 'GORILLA', speed: 235, jump: 540, hp: 6, rageGain: 1.0, scale: 1.02, blurb: 'Balanced wrecking machine' },
+    croc:    { name: 'CROC', speed: 270, jump: 515, hp: 5, rageGain: 1.08, scale: 1.00, blurb: 'Fast and savage' },
+    cow:     { name: 'COW', speed: 215, jump: 500, hp: 8, rageGain: 0.92, scale: 1.03, blurb: 'Slow. Heavy. Unreasonable.' },
+    eagle:   { name: 'EAGLE', speed: 250, jump: 610, hp: 5, rageGain: 1.0, scale: 0.96, blurb: 'Air control specialist' },
+    trex:    { name: 'T-REX', speed: 230, jump: 485, hp: 7, rageGain: 1.04, scale: 1.08, blurb: 'Maximum bite force' }
+  },
+  palette: {
+    ink: 0x06020a,
+    pink: 0xff2bd6,
+    cyan: 0x35e7ff,
+    lime: 0xbaff39,
+    orange: 0xff8a21,
+    white: 0xffffff,
+    red: 0xff3d55
+  }
+};
