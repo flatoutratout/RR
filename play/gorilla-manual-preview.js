ff2b7f03f11bbb285449213d82efab89054a1779
flatoutratout <flatoutwithyaratout@gmail.com>
2026-09-08 14:13:18 +0100

    Add manual Gorilla preview config

---
+++ play/gorilla-manual-preview.js
@@ -0,0 +1,6 @@
+// Manual 500x500 Gorilla sprite set: use one common visual scale and anchor.
+(function(){
+  const SCALE=0.26;
+  const Y_OFFSET=65;
+  window.RR_GORILLA_MANUAL={scale:SCALE,yOffset:Y_OFFSET};
+})();
